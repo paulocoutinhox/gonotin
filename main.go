8d98bee47029af036a6840db6e1ee6fcd03d35ff
Paulo Coutinho <paulo@prsolucoes.com>
2015-10-14 20:47:10 -0300

    golang script with two modes now

--- main.go
+++ main.go
@@ -8,52 +8,106 @@ import (
 )
 
 func main() {
-	// general data
-	var dataA []string
-	var dataB []string
+	var mode = 2
 
-	// read file A
-	fileA, errA := os.Open(os.Args[1])
+	if mode == 1 {
 
-	if errA != nil {
-		log.Fatal(errA)
-	}
+		// general data
+		var dataA []string
+		var dataB []string
 
-	defer fileA.Close()
+		// read file A
+		fileA, errA := os.Open(os.Args[1])
 
-	scannerA := bufio.NewScanner(fileA)
-
-	for scannerA.Scan() {
-		dataA = append(dataA, scannerA.Text())
-	}
-
-	// read file B
-	fileB, errB := os.Open(os.Args[2])
-
-	if errB != nil {
-		log.Fatal(errB)
-	}
-
-	defer fileB.Close()
-
-	scannerB := bufio.NewScanner(fileB)
-
-	for scannerB.Scan() {
-		dataB = append(dataB, scannerB.Text())
-	}
-
-	// debug
-	fmt.Println("> Data in A: ", len(dataA), " | Data in B: ", len(dataB))
-
-	// process and show data from A that not exists in B
-	total := len(dataB)
-	sort.Strings(dataB)
-	
-	for _, valueA := range dataA {
-		pos := sort.SearchStrings(dataB, valueA)
-
-		if !(pos < total && dataB[pos] == valueA) {
-			fmt.Println(valueA)
+		if errA != nil {
+			log.Fatal(errA)
 		}
+
+		defer fileA.Close()
+
+		scannerA := bufio.NewScanner(fileA)
+
+		for scannerA.Scan() {
+			dataA = append(dataA, scannerA.Text())
+		}
+
+		// read file B
+		fileB, errB := os.Open(os.Args[2])
+
+		if errB != nil {
+			log.Fatal(errB)
+		}
+
+		defer fileB.Close()
+
+		scannerB := bufio.NewScanner(fileB)
+
+		for scannerB.Scan() {
+			dataB = append(dataB, scannerB.Text())
+		}
+
+		// debug
+		fmt.Println("> Data in A: ", len(dataA), " | Data in B: ", len(dataB))
+
+		// process and show data from A that not exists in B
+		total := len(dataB)
+		sort.Strings(dataB)
+
+		for _, valueA := range dataA {
+			pos := sort.SearchStrings(dataB, valueA)
+
+			if !(pos < total && dataB[pos] == valueA) {
+				fmt.Println(valueA)
+			}
+		}
+
+	} else if mode == 2 {
+
+		// general data
+		var dataA = make(map[string]string)
+		var dataB = make(map[string]string)
+
+		// read file A
+		fileA, errA := os.Open(os.Args[1])
+
+		if errA != nil {
+			log.Fatal(errA)
+		}
+
+		defer fileA.Close()
+
+		scannerA := bufio.NewScanner(fileA)
+
+		for scannerA.Scan() {
+			dataA[scannerA.Text()] = scannerA.Text()
+		}
+
+		// read file B
+		fileB, errB := os.Open(os.Args[2])
+
+		if errB != nil {
+			log.Fatal(errB)
+		}
+
+		defer fileB.Close()
+
+		scannerB := bufio.NewScanner(fileB)
+
+		for scannerB.Scan() {
+			dataB[scannerB.Text()] = scannerB.Text()
+		}
+
+		// debug
+		fmt.Println("> Data in A: ", len(dataA), " | Data in B: ", len(dataB))
+
+		// process and show data from A that not exists in B
+		for _, valueA := range dataA {
+			_, exists := dataB[valueA]
+
+			if !exists {
+				fmt.Println(valueA)
+			}
+		}
+
 	}
 }
