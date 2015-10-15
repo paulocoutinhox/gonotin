04021f960616289a80b9fe6286d3dfd19d29fba1
Paulo Coutinho <paulo@prsolucoes.com>
2015-10-15 15:00:16 -0300

    new cpp version of Rajanikanth from go-nuts

--- cpp-version/main.cpp
+++ cpp-version/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
     // 1 = slow using std::vector
     // 2 = using set
     // 3 = using std::unordered_set - thanks for 0xe2.0x9a.0x9b from go-nuts
+    // 4 = using std::set_difference - thanks for Rajanikanth from go-nuts - it not show the correct data
 
     int mode = 3;
 
@@ -154,6 +155,54 @@ int main(int argc, char *argv[])
             {
                 printf("%s\n", value.c_str());
             }
+        }
+    }
+    else if (mode == 4)
+    {
+        // version from: Rajanikanth
+
+        // general
+        std::set<string> result;
+
+        // get data from file A
+        std::unordered_set<string> dataA;
+        ifstream streamA;
+
+        streamA.open(argv[1]);
+
+        while(!streamA.eof())
+        {
+            string line;
+            streamA >> line;
+            dataA.insert(line);
+        }
+
+        streamA.close();
+
+        // get data from file B
+        std::unordered_set<string> dataB;
+        ifstream streamB;
+
+        streamB.open(argv[2]);
+
+        while(!streamB.eof())
+        {
+            string line;
+            streamB >> line;
+            dataB.insert(line);
+        }
+
+        streamB.close();
+
+        // show debug data
+        printf("> Data in A: %zu | Data in B: %zu\n", dataA.size(), dataB.size());
+
+        std::set_difference(dataA.begin(), dataA.end(), dataB.begin(), dataB.end(), inserter(result, result.end()));
+
+        // process data
+        for (auto elem: result)
+        {
+            printf("%s\n", elem.c_str());
         }
     }
 }
