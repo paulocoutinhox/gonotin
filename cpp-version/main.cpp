ede342d4ba0e1831a873c810faacac025c4881aa
Paulo Coutinho <paulo@prsolucoes.com>
2015-10-15 14:26:20 -0300

    new cpp version from 0xe2.0x9a.0x9b of go-nuts group

--- cpp-version/main.cpp
+++ cpp-version/main.cpp
@@ -3,13 +3,18 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <unordered_set>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // select mode (1 = slow using std::vector, 2 = using set)
-    int mode = 2;
+    // select mode
+    // 1 = slow using std::vector
+    // 2 = using set
+    // 3 = using std::unordered_set - thanks for 0xe2.0x9a.0x9b from go-nuts
+
+    int mode = 3;
 
     if (mode == 1)
     {
@@ -59,7 +64,6 @@ int main(int argc, char *argv[])
     }
     else if (mode == 2)
     {
-        // general
         // get data from file A
         std::set<string> dataA;
         ifstream streamA;
@@ -77,6 +81,54 @@ int main(int argc, char *argv[])
 
         // get data from file B
         std::set<string> dataB;
+        ifstream streamB;
+
+        streamB.open(argv[2]);
+
+        while(!streamB.eof())
+        {
+            string line;
+            streamB >> line;
+            dataB.insert(line);
+        }
+
+        streamB.close();
+
+        // show debug data
+        printf("> Data in A: %zu | Data in B: %zu\n", dataA.size(), dataB.size());
+
+        // process data
+        for(string value : dataA)
+        {
+            auto search = dataB.find(value);
+
+            if (search == dataB.end())
+            {
+                printf("%s\n", value.c_str());
+            }
+        }
+    }
+    else if (mode == 3)
+    {
+        // version from: 0xe2.0x9a.0x9b
+
+        // get data from file A
+        std::unordered_set<string> dataA;
+        ifstream streamA;
+
+        streamA.open(argv[1]);
+
+        while(!streamA.eof())
+        {
+            string line;
+            streamA >> line;
+            dataA.insert(line);
+        }
+
+        streamA.close();
+
+        // get data from file B
+        std::unordered_set<string> dataB;
         ifstream streamB;
 
         streamB.open(argv[2]);
