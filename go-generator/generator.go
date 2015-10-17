adb07f4c11454f2776b5bdbc229f532399a79157
Paulo Coutinho <paulo@prsolucoes.com>
2015-10-16 22:52:23 -0300

    new generator from Michael Jones (go-nuts)

--- go-generator/generator.go
+++ go-generator/generator.go
@@ -1,44 +1,49 @@
 package main
+
 import (
-	"os"
-	"log"
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"strconv"
 	"time"
-	"strings"
 )
 
+func usage() {
+	fmt.Printf("usage: generate filename count max\n")
+	os.Exit(1)
+}
+
 func random(min, max int) int {
-	return rand.Intn(max - min) + min
+	return rand.Intn(max-min) + min
 }
 
 func main() {
-	// general data
-	rand.Seed(int64(time.Now().Nanosecond()))
+	if len(os.Args) != 4 {
+		usage()
+	}
 	filename := os.Args[1]
+	count, e2 := strconv.Atoi(os.Args[2])
+	max, e3 := strconv.Atoi(os.Args[3])
+	if len(filename) < 1 || count < 0 || e2 != nil || max < 0 || e3 != nil {
+		usage()
+	}
 
-	totalNumbersArg, _ := strconv.ParseInt(os.Args[2], 10, 0)
-	totalNumbers := int(totalNumbersArg)
+	// generate a different sequence every time (if desired)
+	rand.Seed(int64(time.Now().Nanosecond()))
 
-	maxNumberArg, _ := strconv.ParseInt(os.Args[3], 10, 0)
-	maxNumber := int(maxNumberArg)
-
-	// generate data for file
+	// create output file
 	file, err := os.Create(filename)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// random number
-	for x := 0; x < totalNumbers; x++ {
-		var number = random(0, maxNumber)
-		file.Write([]byte(strconv.Itoa(number)))
-		file.WriteString("\n")
+	buffer := bufio.NewWriter(file)
+	for i := 0; i < count; i++ {
+		fmt.Fprintln(buffer, random(0, max))
 	}
+	buffer.Flush()
+	file.Close()
 
-	defer file.Close()
-
-	fmt.Println("> File generated at: ", strings.Trim(filename, " "))
+	fmt.Printf("wrote %d numbers in (0 ≤ n < %d) to file %q\n", count, max, filename)
 }
