9b52b834d1c88646a00b2d42eeceb1e32ab9d474
Paulo Coutinho <paulo@prsolucoes.com>
2015-10-21 01:42:14 -0200

    script used to install deps on debian - raspberry

---
+++ install-gonotin-deps-debian.sh
@@ -0,0 +1,11 @@
+echo "> Installing C++..."
+apt-get install build-essential
+echo ""
+
+echo "> Installing Go..."
+sudo apt-get install golang
+echo ""
+
+echo "> Installing Python..."
+sudo apt-get install python
+echo ""
